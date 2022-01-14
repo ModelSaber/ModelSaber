783771f8ac54764030b0f7b6ffc3f117f2ae5112
WildWolf <magnus1997.mho@gmail.com>
2022-01-14 21:49:02 +0100

    Separate database models to its own library project

---
+++ ModelSaber.Models/BaseId.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelSaber.Models
+{
+    public class BaseId
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ ModelSaber.Models/Tag.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ModelSaber.Models
+{
+    public class Tag : BaseId
+    {
+        public Guid CursorId { get; set; }
+        public string Name { get; set; } = null!;
+        [JsonIgnore]
+        public virtual ICollection<ModelTag> ModelTags { get; set; } = null!;
+    }
+
+    public class ModelTag : BaseId
+    {
+        public int ModelId { get; set; }
+        public int TagId { get; set; }
+        public virtual Tag Tag { get; set; } = null!;
+        [JsonIgnore]
+        public virtual Model Model { get; set; } = null!;
+    }
+}
