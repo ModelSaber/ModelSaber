f03eef130f3fe754c404a735f048244dcce92762
WildWolf <magnus1997.mho@gmail.com>
2022-01-30 18:32:48 +0100

    Add model types

---
+++ ModelSaber.Models/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ModelSaber.Models
+{
+    public static class Extensions
+    {
+        public static void Shuffle<T>(this IEnumerable<T> enumerable)
+        {
+            var provider = new RNGCryptoServiceProvider();
+            var list = enumerable.ToArray();
+            var n = list.Length;
+            while (n > 1)
+            {
+                var box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (byte.MaxValue / n)));
+                var k = box[0] % n;
+                n--;
+                (list[k], list[n]) = (list[n], list[k]);
+            }
+        }
+
+        public static bool ToBool(this bool? b) => b.HasValue && b.Value;
+    }
+}
---
+++ ModelSaber.Models/OAuthClient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ModelSaber.Models
+{
+    public class OAuthClient : BaseId
+    {
+        public Guid ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public byte[] SecKey { get; set; }
+        public string? RedirectUri { get; set; }
+        public string? Scope { get; set; }
+        public virtual List<OAuthToken> Tokens { get; set; }
+
+        public void InitSecKey()
+        {
+            SecKey = getKey();
+        }
+
+        public void RotateSecret()
+        {
+            ClientSecret = Convert.ToBase64String(getKey());
+            InitSecKey();
+            Tokens.Clear();
+        }
+
+        private byte[] getKey()
+        {
+            using var aes = Aes.Create("AesCryptoServiceProvider");
+            aes.GenerateKey();
+            return aes.Key;
+        }
+
+        public OAuthToken GetToken()
+        {
+            using var sha = SHA512.Create();
+            var utcNow = DateTime.UtcNow;
+            var bytes = new[] { BitConverter.GetBytes(utcNow.ToBinary()), ClientId.ToByteArray(), SecKey }.SelectMany(t => t).ToArray();
+            bytes.Shuffle();
+            return new OAuthToken
+            {
+                ClientId = Id,
+                Token = sha.ComputeHash(bytes)
+            };
+        }
+    }
+
+    public class OAuthToken : BaseId
+    {
+        public uint ClientId { get; set; }
+        public byte[] Token { get; set; }
+        public DateTime InsertedAt { get; set; }
+        public uint ExpiresIn { get; set; }
+        public virtual OAuthClient Client { get; set; }
+        public byte[] Refresh { get; set; }
+        public string? Scope { get; set; }
+
+        public bool IsExpired()
+        {
+            return DateTime.UtcNow > InsertedAt.AddSeconds(ExpiresIn);
+        }
+
+        public IEnumerable<string> GetScopes() => new[] { "public" }.Concat(Scope?.Split(' ') ?? Array.Empty<string>());
+    }
+}
